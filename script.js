6de5abcc82e2ac4f6500125a409a97a68b82c7bc
ChietruProgramatoru <petruneamtu000@gmail.com>
2023-07-25 15:59:24 +0300

    Calculator & History works, no order of operation yet

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 let clear = 0;
 var userInput = [];
 var mergedInput = [];
-var currentNumber = 0;
 var solution = 0;
+var finishedSolution = 0;
+// var add = (a,b) => a + b;
+// var sub = (a,b) => a - b;
+// var div = (a,b) => a / b;
+// var mul = (a,b) => a * b;
+
+// function allCalc() {
+//     if (!Number.isInteger(mergedInput[0])){
+//         if (mergedInput[0] == '+') {
+//             solution = finishedSolution + mergedInput[1];
+//         }
+//         if (mergedInput[0] == '-') {
+//             solution = finishedSolution - mergedInput[1];
+//         }
+//         if (mergedInput[0] == '*') {
+//             solution = finishedSolution * mergedInput[1];
+//         }
+//         if (mergedInput[0] == '/') {
+//             solution = finishedSolution / mergedInput[1];
+//         }
+//         mergedInput.shift();
+//     }
+//     if (Number.isInteger(mergedInput[0])) {
+//         solution = mergedInput[0];
+//         while (mergedInput[0] != null) {
+//             if (Number.isInteger(mergedInput[0])) {
+//                 if (mergedInput[1] == '+') {
+//                     solution = solution + mergedInput[2];
+//                 }
+//                 if (mergedInput[1] == '-') {
+//                     solution = solution - mergedInput[2];
+//                 }
+//                 if (mergedInput[1] == '*') {
+//                     solution = solution * mergedInput[2];
+//                 }
+//                 if (mergedInput[1] == '/') {
+//                     solution = solution / mergedInput[2];
+//                 }
+//                 mergedInput.shift();
+//             }
+
+//             else {
+//                 mergedInput.shift();
+//             }
+//         }
+//     }
+// }
 
 // const del = document.getElementById(`del`);
 // del.addEventListener(`click`, function() {
@@ -17,7 +63,9 @@ const equals = document.getElementById(`equals`);
 equals.addEventListener(`click`, function() {
     calculation();
     var result = document.getElementById(`result`);
-    result.innerHTML = `${solution}`;
+    var history = document.getElementById(`history`);
+    history.innerHTML = `${result.innerHTML}`;
+    result.innerHTML = `${finishedSolution}`;
 });
 
 const pressedButton = document.getElementsByClassName(`button`);
@@ -25,15 +73,21 @@ for (var i=0; i < 16; ++i) {
     (function(index) {
         pressedButton[index].addEventListener("click", function() {
             console.log(this.innerHTML);
-            if (['1','2','3','4','5','6','7','8','9','0'].includes(this.innerHTML)) {
-                userInput.push(parseInt(this.innerHTML));
+            var result = document.getElementById(`result`);
+            if (this.innerHTML == '=') return;
+            else {
+                result.innerHTML += `${this.innerHTML}`;
+                if (['1','2','3','4','5','6','7','8','9','0'].includes(this.innerHTML)) {
+                    userInput.push(parseInt(this.innerHTML));
+                }
+                else userInput.push(this.innerHTML);
             }
-            else userInput.push(this.innerHTML);
         });
     })(i);
 };
 
 function merge() {
+    var currentNumber = 0;
     while (userInput[0] != null) {
         if (Number.isInteger(userInput[0])) {
             currentNumber = currentNumber*10 + userInput[0];
@@ -51,27 +105,52 @@ function merge() {
 
 function calculation() {
     merge();
-    solution = mergedInput[0];
-    while (mergedInput[0] != null) {
-        if (Number.isInteger(mergedInput[0])) {
-            if (mergedInput[1] == '+') {
-                solution = solution + mergedInput[2];
-            }
-            if (mergedInput[1] == '-') {
-                solution = solution - mergedInput[2];
-            }
-            if (mergedInput[1] == '*') {
-                solution = solution * mergedInput[2];
-            }
-            if (mergedInput[1] == '/') {
-                solution = solution / mergedInput[2];
-            }
-            mergedInput.shift();
+    // for (i = 0; i < mergedInput.length; i++) {
+    //     if (mergedInput[i] == '(') {
+    //         var j = i;
+    //         while (mergedInput[i]){}
+    //     }
+    }
+    if (!Number.isInteger(mergedInput[0])){
+        if (mergedInput[0] == '+') {
+            solution = finishedSolution + mergedInput[1];
         }
+        if (mergedInput[0] == '-') {
+            solution = finishedSolution - mergedInput[1];
+        }
+        if (mergedInput[0] == '*') {
+            solution = finishedSolution * mergedInput[1];
+        }
+        if (mergedInput[0] == '/') {
+            solution = finishedSolution / mergedInput[1];
+        }
+        mergedInput.shift();
+    }
+    if (Number.isInteger(mergedInput[0])) {
+        solution = mergedInput[0];
+        while (mergedInput[0] != null) {
+            if (Number.isInteger(mergedInput[0])) {
+                if (mergedInput[1] == '+') {
+                    solution = solution + mergedInput[2];
+                }
+                if (mergedInput[1] == '-') {
+                    solution = solution - mergedInput[2];
+                }
+                if (mergedInput[1] == '*') {
+                    solution = solution * mergedInput[2];
+                }
+                if (mergedInput[1] == '/') {
+                    solution = solution / mergedInput[2];
+                }
+                mergedInput.shift();
+            }
 
-        else {
-            mergedInput.shift();
+            else {
+                mergedInput.shift();
+            }
         }
     }
-    console.log(solution);
+    finishedSolution += solution;
+    solution = 0;
+    return finishedSolution;
 }
